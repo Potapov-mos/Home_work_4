90cadc9b9b2a07d567f0e12a6c43b36bceb39cf3
Dmitriy <potapidze.mos@mail.ru>
2022-10-14 19:17:50 +0300

    Добавил решение задачи №29

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -4,3 +4,30 @@
 //и выводит их на экран.
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int[] FillArray(int num)
+{
+    int[] array = new int[num];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) 
+{ 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        Console.Write(array[i]); 
+        if (i < array.Length - 1) 
+        { 
+            Console.Write(", ");
+        } 
+    } 
+} 
+
+int[] myArr = FillArray(8);
+PrintArray(myArr);
